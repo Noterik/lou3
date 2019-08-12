e6a6a5fc73a20d817b3fd4bde24cd0d974f9118e
Daniel Ockeloen <daniel@noterik.nl>
2019-08-12 14:51:41 +0200

    backsup sync

--- src/org/springfield/lou/ServiceHandler.java
+++ src/org/springfield/lou/ServiceHandler.java
@@ -1,14 +1,17 @@
 package org.springfield.lou;
 
 import org.springfield.lou.application.ApplicationManager;
+import org.springfield.lou.model.*;
 import org.springfield.lou.homer.LazyHomer;
+import org.springfield.lou.model.ModelEventManager;
 import org.springfield.mojo.interfaces.ServiceInterface;
 import org.springfield.mojo.interfaces.ServiceManager;
+import org.springfield.fs.FsNode;
 
 public class ServiceHandler implements ServiceInterface{
-	
+
 	private static ServiceHandler instance;
-	
+
 	public static ServiceHandler instance() {
 		if (instance==null) {
 			instance = new ServiceHandler();
@@ -16,13 +19,12 @@ public class ServiceHandler implements ServiceInterface{
 		}
 		return instance;
 	}
-	
+
 	public String getName() {
 		return "lou";
 	}
-	
+
 	public String get(String uri,String fsxml,String mimetype) {
-		System.out.println("SH="+uri);
 		int pos = uri.indexOf("(");
 		if (pos!=-1) {
 			String command = uri.substring(0,pos);
@@ -33,25 +35,45 @@ public class ServiceHandler implements ServiceInterface{
 		}
 		return null;
 	}
-	
-	public String put(String path,String value,String mimetype) {
+
+	public String put(String uri,String value,String mimetype) {
+		int pos = uri.indexOf("(");
+		if (pos!=-1) {
+			String command = uri.substring(0,pos);
+			String path = uri.substring(pos+1);
+			path = path.substring(0,path.length()-1);
+			if (command.equals("notify")) {
+				ModelEventManager em = Model.getEventManager();
+				FsNode msg = new FsNode("msg","1");
+				String[] list = value.split(",");
+				if (list.length>0) {
+					for (int i=0;i<list.length;i++) {
+						pos = list[i].indexOf("=");
+						if (pos!=-1) {
+							msg.setProperty(list[i].substring(0,pos),list[i].substring(pos+1));
+						}
+					}
+				}			
+				em.notify(path,msg);
+			}
+		}
 		return null;
 	}
-	
+
 	public String post(String path,String fsxml,String mimetype) {
 		return null;
 	}
-	
+
 	public String delete(String path,String value,String mimetype) {
 		return null;
 	}
-	
+
 	private String handleGetCommand(String command,String[] params) {
-		System.out.println("HG="+command);
+		//System.out.println("HG="+command);
 		if (command.equals("getAppWAR")) return getAppWar(params[0],params[1]);
 		return null;
 	}
-	
+
 	private String getAppWar(String appname,String version) {
 		String result = ApplicationManager.getApplicationWarAsString(appname,version);
 		if (result!=null) {
