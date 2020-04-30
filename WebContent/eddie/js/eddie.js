1da5f653910a5da12f1352a30e39207bb3d5428c
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2020-04-30 11:27:41 +0200

    Update eddie.js
    cookie domain set to qandr.eu

--- WebContent/eddie/js/eddie.js
+++ WebContent/eddie/js/eddie.js
@@ -1342,7 +1342,7 @@ var Eddie = function(options){
     	} else {
         	expires = "";
     	}
-   	 	document.cookie = escape(name) + "=" + escape(value) + expires + "; domain=.mupop.net;path=/";
+   	 	document.cookie = escape(name) + "=" + escape(value) + expires + "; domain=.qandr.eu;path=/";
 	}
 
 	function readCookie(name) {
